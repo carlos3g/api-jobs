815f89c419480ebc58353633d5053e7e102529bf
carlos3g <carlimbr156@gmail.com>
2019-12-12 21:18:18 -0300

    adicionando a funcionalidade de shuffle

--- src/controllers/ReceivedController.js
+++ src/controllers/ReceivedController.js
@@ -3,10 +3,28 @@ const Employers = require('../models/Employers');
 
 // TODO: colocar condição para caso o usuário não for encontrado
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 module.exports = {
   async index(req, res) {
     const { id, type } = req.headers;
-
 
     if (type === 'em') {
       const employer = await Employers.findById(id).catch(() => { console.log(`Erro: Empresa não encontrada`) });
@@ -27,6 +45,9 @@ module.exports = {
         {_id: {$nin: unemployer.send}} // pega todos os employers que não estão em send
       ]
     })
+
+    shuffle(ems)
+
     return res.json(ems);
   }
 
